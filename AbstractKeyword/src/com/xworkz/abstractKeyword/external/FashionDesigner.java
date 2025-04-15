4e254ba84e5f136efc28083aef98a65c50f2b73f
Sonika-VU <varadasonikau31@gmail.com>
2025-04-15 18:40:54 +0530

    Adding FashionDesigner class file

---
+++ AbstractKeyword/src/com/xworkz/abstractKeyword/external/FashionDesigner.java
@@ -0,0 +1,10 @@
+package com.xworkz.abstractKeyword.external;
+
+public abstract class FashionDesigner {
+
+    public abstract void design();
+
+    public void teach(){
+        System.out.println("Running teach in FashionDesigner");
+    }
+}
