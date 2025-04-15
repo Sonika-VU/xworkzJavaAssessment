dd98b85244b086f8b95260def6aaa4f5229c2996
Sonika-VU <varadasonikau31@gmail.com>
2025-04-15 18:42:13 +0530

    Adding Home.java file

---
+++ AbstractKeyword/src/com/xworkz/abstractKeyword/internal/Home.java
@@ -0,0 +1,15 @@
+package com.xworkz.abstractKeyword.internal;
+
+import com.xworkz.abstractKeyword.external.Building;
+
+public class Home extends Building {
+
+    public Home(String name, String location, int cost) {
+        super(name, location, cost);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
